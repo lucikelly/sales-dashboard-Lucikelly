d0833ddeaf4269f28faccc8c669e361910586b6c
lucikelly <lucikelly2001@gmail.com>
2024-09-11 15:46:03 -0300

    Feat: Create Themes

---
+++ src/types/theme.d.ts
@@ -0,0 +1,42 @@
+export interface Theme {
+  appBackground: string
+  appColor: string
+  aooDefaultStroke: string
+  appLogo: string
+  appSkeletonFrom: string
+  appSkeletonTo: string
+  buttons: {
+    alert: string
+    alertColor: string
+    alertHover: string
+    disabled: string
+    disabledColor: string
+    primary: string
+    primaryColor: string
+    primaryHover: string
+  }
+
+  card: {
+    alert: string
+    background: string
+    border: string
+    success: string
+    warning: string
+  }
+
+  textInput: {
+    active: string
+    activeColor: string
+    borderColor: string
+    disabled: string
+    disabledBoderColor: string
+    disebledColor: string
+    placeholderColor: string
+  }
+
+  typographies: {
+    error: string
+    subtitle: string
+    success: string
+  }
+}
