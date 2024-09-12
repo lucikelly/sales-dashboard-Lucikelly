d9235ae8fd25c673dee42a70d49b1d1864622037
lucikelly <lucikelly2001@gmail.com>
2024-09-11 22:43:25 -0300

    Feat: Form component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
-import { BannerImage } from "@/components";
+import { BannerImage, FormComponent, StyledH1, StyledP, Logo } from "@/components";
+import { pxToRem } from "@/utils";
 import { Box, Container, Grid } from "@mui/material";
 
 
@@ -15,18 +16,35 @@ function Login () {
            sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
          >
            <Container maxWidth="sm">
-             <h1> Login </h1>
+            <Box sx={{ marginBottom: pxToRem(24)}}>
+              <Logo height={41} width={100}/>
+            </Box>
+             <Box sx={{ marginBottom: pxToRem(24)}}>
+               <StyledH1>Bem-vindo</StyledH1>
+               <StyledP> Digite sua senha e email para logar</StyledP>
+             </Box>
+
+             <FormComponent
+               inputs={[
+                 { type: 'email', placeholder: 'Email' },
+                 { type: 'password', placeholder: 'Senha' },
+               ]}
+               buttons={[
+                 {
+                   className: 'primary',
+                   type: 'submit',
+                   children: 'Login',
+                 },
+               ]}
+               message={{
+                 msg: 'deu bommm!!',
+                 type: 'success',
+               }}
+             />
            </Container>
          </Grid>
-         <Grid
-           item
-          
-           sm={6}
-           sx={{ display: { xs: 'none', sm: 'block'} }}
-         >
-           
-             <BannerImage/>
-           
+         <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
+           <BannerImage />
          </Grid>
        </Grid>
      </Box>
