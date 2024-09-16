6cfc12c8f54d6bcaf5a97062dca78ec4c086a858
lucikelly <lucikelly2001@gmail.com>
2024-09-16 13:00:57 -0300

    Feat: Login Page Requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from "react-router-dom"
+import Cookies from "js-cookie"
 import { Home, Login, Registration, Leads, Profile } from "./pages"
 
 function App() {
+  const ProtectedRoute = () => {
+    const checkAuthCookies = Cookies.get('Authorization')
+    if (!checkAuthCookies) {
+      alert('Autenticação necessária')
+      return <Navigate to="/" replace />
+    }
+
+    return <Outlet />
+  }
+
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/cadastro" element={<Registration/>} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="/leads" element={<Leads/>} />
-          <Route path="/perfil" element={<Profile/>} />
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<Registration />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/leads" element={<Leads />} />
+            <Route path="/perfil" element={<Profile />} />
+          </Route>
         </Routes>
       </Router>
     </>
